fd170d74f83dce100e3f3c8cfa7b49f7cc6b5788
Cesar-Tapuio <cesarmoritu@gmail.com>
2026-03-28 10:33:56 -0300

    first commit

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+operador = input("Digite a operação (+, -, *, /): ")
+
+if operador == "+":
+    resultado = numero1 + numero2
+    print("O resultado da adição é:", resultado)
+
+elif operador == "-":
+    resultado = numero1 - numero2
+    print("O resultado da subtração é:", resultado)
+
+elif operador == "*":
+    resultado = numero1 * numero2
+    print("O resultado da multiplicação é:", resultado)
+
+elif operador == "/":
+    if numero2 != 0:
+        resultado = numero1 / numero2
+        print("O resultado da divisão é:", resultado)
+    else:
+        print("Erro: Divisão por zero não é permitida.")
