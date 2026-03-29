07d0047e63f046f0dded12bd8fca954098de9794
Cesar-Tapuio <cesarmoritu@gmail.com>
2026-03-29 20:51:35 -0300

    Edição de txt

--- Aprendendo Python/Importando arquivos/Teste - Calculadora/TesteImportarAquivos.py
+++ Aprendendo Python/Importando arquivos/Teste - Calculadora/TesteImportarAquivos.py
@@ -1,7 +1,7 @@
 numeros = []
 soma = 0
 
-with open("/home/cesar/Documents/Python - Testes/Aprendendo/Aprendendo Python/Importando arquivos/input.txt", "r") as arquivo:
+with open("/home/cesar/Documents/Python - Testes/Aprendendo/Aprendendo Python/Importando arquivos/Teste - Calculadora/input.txt", "r") as arquivo:
     for linha in arquivo:
         numeros.append(int(linha.strip()))
 
---
+++ Aprendendo Python/Importando arquivos/Teste - Login/login.py
@@ -0,0 +1,53 @@
+
+
+def cadastro():
+    print("Cadastro de usuário")
+    nome = input("Digite seu nome: ")
+    email = input("Digite seu email: ")
+    senha = input("Digite sua senha: ")
+
+    with open("/home/cesar/Documents/Python - Testes/Aprendendo/Aprendendo Python/Importando arquivos/Teste - Login/usuarios.txt", "a") as arquivo:
+        arquivo.write(f"{nome},{email},{senha}\n")
+
+    print(f"Usuário {nome} cadastrado com sucesso!")
+
+
+
+
+def login():
+    nome = input("Digite seu nome de usuário: ")
+    senha = input("Digite sua senha: ")
+    dados = []
+    
+    with open("/home/cesar/Documents/Python - Testes/Aprendendo/Aprendendo Python/Importando arquivos/Teste - Login/usuarios.txt", "r") as arquivo:
+        #Procurar resgistros do usuario
+        for dado in arquivo:
+            if dado.strip().split(",")[0] == nome:
+                if dado.strip().split(",")[2] == senha:
+                    print("Login feito com sucesso!")
+                    break
+            
+
+
+
+def menu():
+    while True:
+        
+        print("\n1 - Cadastrar usuário")
+        print("2 - Login")
+        print("3 - Sair")
+        
+        escolha = input("Escolha uma opção: ")
+        
+        if escolha == "1":
+            cadastro()
+        elif escolha == "2":
+            login()
+
+        elif escolha == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+menu()
