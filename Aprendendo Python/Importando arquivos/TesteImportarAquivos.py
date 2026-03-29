3ad6daee6c3b41320857159a68241786a0f162b3
Cesar-Tapuio <cesarmoritu@gmail.com>
2026-03-29 17:30:38 -0300

    Importando arquivos

---
+++ Aprendendo Python/Importando arquivos/TesteImportarAquivos.py
@@ -0,0 +1,11 @@
+numeros = []
+soma = 0
+
+with open("/home/cesar/Documents/Python - Testes/Aprendendo/Aprendendo Python/Importando arquivos/input.txt", "r") as arquivo:
+    for linha in arquivo:
+        numeros.append(int(linha.strip()))
+
+for numero in numeros:
+    soma += numero
+
+print(f"A soma dos números é: {soma}")
